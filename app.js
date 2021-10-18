5e4d0a7bfe37d42f07084a5bc66b134981c5a8b9
jongwanra <najongsi@gmail.com>
2021-10-18 14:53:56 +0900

    [211018] Add cors, compression setting

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const compression = require('compression');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const indexRouter = require('./routes/index');
+const app = express();
+require('dotenv').config();
+app.use(compression()); // 요청받은 내용들 압축
 
-var indexRouter = require('./routes/index');
+//CORS
+const cors = require('cors');
+const corsOptions = {
+  //cors 설정
+  origin: '*', // 전체 허용
+  methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+  preflightContinue: false,
 
-var app = express();
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,12 +36,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
