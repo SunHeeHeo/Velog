1ca95ad5330823ac801911328b5e177d24e5eceb
yunjaejo <1a616@naver.com>
2021-10-21 15:25:35 +0900

    [211021] 회원가입 에러메세지 변경

--- routes/user.js
+++ routes/user.js
@@ -31,7 +31,7 @@ router.post('/signup', async (req, res) => {
       console.log('email이 중복 되어 있습니다.');
       return res.status(400).json({
         success: false,
-        errMessage: '이메일이 중복되었습니다.',
+        errMessage: '이미 존재하는 이메일입니다.',
       });
     }
     // userNickname 중복 여부
@@ -39,7 +39,7 @@ router.post('/signup', async (req, res) => {
       console.log('닉네임이 중복 되어 있습니다.');
       return res.status(400).json({
         success: false,
-        errMessage: '닉네임이 중복되었습니다.',
+        errMessage: '이미 존재하는 닉네임입니다.',
       });
     }
 
