ef49b880865f17cfa0ad43571c6c23d634b60e1b
yunjaejo <1a616@naver.com>
2021-10-22 14:12:24 +0900

    [211022] 상세페이지에 postImage 추가전달

--- routes/post.js
+++ routes/post.js
@@ -71,7 +71,7 @@ router.get('/:postId', async (req, res) => {
 
   const query = `
   select 
-  post.postId, comment.commentId, postTitle, postContent, post.userNickname as postUserNickname, postTime, commentContent, 
+  post.postId, comment.commentId, postImage, postTitle, postContent, post.userNickname as postUserNickname, postTime, commentContent, 
   comment.userNickname as commentUserNickname, commentTime from post 
   left join comment on post.postId = comment.postId WHERE post.postId=${postId}`;
 
@@ -187,7 +187,8 @@ function getDetailPostData(rows) {
     postTitle: rows[0].postTitle,
     postContent: rows[0].postContent,
     postTime: rows[0].postTime,
-    postUserNickname: rows[0].postUserNickname
+    postUserNickname: rows[0].postUserNickname,
+    postImage: rows[0].postImage
   };
 }
 
